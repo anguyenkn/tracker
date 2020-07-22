6c18ed2ad49ff7760ad6b85b997e73375742babc
Aidan Nguyen <anguyenkn@gmail.com>
2020-07-22 06:36:07 -0700

    added signout functionality

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -11,6 +11,8 @@ const authReducer = (state, action) => {
       return { token: action.payload, errorMessage: '' };
     case 'clear_error_message':
       return { ...state, errorMessage: '' };
+    case 'sign_out':
+      return { token: null, errorMessage: '' };
     default:
       return state;
   }
@@ -60,8 +62,10 @@ const signin = (dispatch) => async ({ email, password }) => {
   }
 };
 
-const signout = (dispatch) => {
-  return () => {};
+const signout = (dispatch) => async () => {
+  await AsyncStorage.removeItem('token');
+  dispatch({ type: 'sign_out' });
+  navigate('loginFlow');
 };
 
 export const { Provider, Context } = createDataContext(
